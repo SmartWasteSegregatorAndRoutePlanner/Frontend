69eff6a2ca1ca3f50ad840599a846f111bb0401c
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2023-04-13 00:15:41 +0530

    implement login page
    refactor static files
    
    fix bugs

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,21 @@
-// $(document).ready(function(){
-//     $('.toggle').click(function(){
-//         $('.menu').toggleClass('active');
-//     });
-// }); // fix error
+function getAuthToken() {
+    let authCookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        let cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            let cookie = cookies[i].trim();
+            if (cookie.substring(0, 9) === "authToken") {
+                authCookieValue = decodeURIComponent(cookie.substring(9 + 1));
+                break;
+            }
+        }
+    }
+
+    // Display the value of "myCookie" in an alert box
+    if (authCookieValue) {
+        return authCookieValue;
+    } else {
+        // Redirect the user to a new page
+        window.location.href = "./login.html";
+    }
+}
