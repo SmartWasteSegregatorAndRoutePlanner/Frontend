e37b93e446a280c52eefa7f7a1204c32652317c8
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2023-04-13 00:29:48 +0530

    add authorization headers in requests from index page

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,22 @@
+function setAuthTokenCookie(authToken) {
+    // avoid XSS
+    authToken = encodeURIComponent(authToken.replace('<', '').replace('>', ''));
+
+    // expiration days
+    const expirationDays = 1;
+    const d = new Date();
+    d.setTime(d.getTime() + (expirationDays * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + d.toUTCString();
+
+    // set cookie
+    const newCookie = "authToken" + "=" + authToken + ";" + expires + ";path=/";
+    if (document.cookie && document.cookie !== ''){
+        document.cookie = document.cookie + ";" + newCookie; 
+    } else {
+        document.cookie = newCookie;
+    }
+}
+
 function getAuthToken() {
     let authCookieValue = null;
     if (document.cookie && document.cookie !== '') {
@@ -5,7 +24,7 @@ function getAuthToken() {
         for (let i = 0; i < cookies.length; i++) {
             let cookie = cookies[i].trim();
             if (cookie.substring(0, 9) === "authToken") {
-                authCookieValue = decodeURIComponent(cookie.substring(9 + 1));
+                authCookieValue = encodeURIComponent(cookie.substring(9 + 1));
                 break;
             }
         }
